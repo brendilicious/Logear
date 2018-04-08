836c281054687d8290a32ded09432bf62a806a9f
Brenda <lezti.bre89@gmail,com>
2018-04-08 02:59:39 -0500

    Creacion de Servicio Auth y Registro de Ususarios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyABShrADNUsnAGYph1lMtURtLIRE1L3eac",
+    authDomain: "logear-5a8dc.firebaseapp.com",
+    databaseURL: "https://logear-5a8dc.firebaseio.com",
+    projectId: "logear-5a8dc",
+    storageBucket: "logear-5a8dc.appspot.com",
+    messagingSenderId: "111762546490"
+  }
 };
